7859e805da075be3945fd41473c16d7e00158b9c
Zooakva <zooakva2@gmail.com>
2024-03-23 11:23:07 +0100

    correct

--- js/task-2.js
+++ js/task-2.js
@@ -1,29 +1,9 @@
-class Storage {
-  #items;
+const galleryList = document.querySelector(".gallery");
 
-  constructor(items) {
-    this.#items = items;
-  }
+const imagesMarkup = images
+  .map((image) => {
+    return `<li><img src="${image.url}" alt="${image.alt}"></li>`;
+  })
+  .join("");
 
-  getItems() {
-    return this.#items;
-  }
-
-  addItem(newItem) {
-    this.#items.push(newItem);
-  }
-
-  removeItem(itemToRemove) {
-    const index = this.#items.indexOf(itemToRemove);
-    if (index !== -1) {
-      this.#items.splice(index, 1);
-    }
-  }
-}
-
-const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
-console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
-storage.addItem("Droid");
-console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
-storage.removeItem("Prolonger");
-console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+galleryList.insertAdjacentHTML("beforeend", imagesMarkup);
--- js/task-3.js
+++ js/task-3.js
@@ -1,33 +1,7 @@
-class StringBuilder {
-  #value;
+const nameInput = document.querySelector("#name-input");
+const nameOutput = document.querySelector("#name-output");
 
-  constructor(initialValue) {
-    this.#value = initialValue;
-  }
-
-  getValue() {
-    return this.#value;
-  }
-
-  padEnd(str) {
-    this.#value += str;
-  }
-
-  padStart(str) {
-    this.#value = str + this.#value;
-  }
-
-  padBoth(str) {
-    this.padStart(str);
-    this.padEnd(str);
-  }
-}
-
-const builder = new StringBuilder(".");
-console.log(builder.getValue()); // "."
-builder.padStart("^");
-console.log(builder.getValue()); // "^."
-builder.padEnd("^");
-console.log(builder.getValue()); // "^.^"
-builder.padBoth("=");
-console.log(builder.getValue()); // "=^.^="
+nameInput.addEventListener("input", (event) => {
+  const trimmedValue = event.target.value.trim();
+  nameOutput.textContent = trimmedValue || "Anonymous";
+});
